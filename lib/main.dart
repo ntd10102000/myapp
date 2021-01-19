7134ffab092814545b355bf506791f61199ea2cb
Tiến Dương <52647821+ntd10102000@users.noreply.github.com>
2021-01-19 23:31:01 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  //tại đây chúng ta có thể viết, nạp , gọi các phương thức cần thiết trước khi runApp (nếu có)
+  runApp(
+    Center(
+        child: Text(
+      'Dương béo chào bạn nghĩa nhá :v',
+      textDirection: TextDirection.ltr,
+    )),
+  );
+}
